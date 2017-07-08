ab7507b935069f1be82002f7af59c0b81117f68b
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 06:43:31 +0300

    Add a stub for the listener

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace SwiftTranslator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			if(args.Length != 2) {
+				Console.WriteLine("usage: SwiftTranslator.exe [input file] [output file]");
+				Environment.Exit(1);
+			}
+
+			string input = File.ReadAllText(args[0]);
+
+			var ms = new MemoryStream(Encoding.UTF8.GetBytes(input));
+			var lexer = new SwiftLexer(new AntlrInputStream(ms));
+
+			var tokens = new CommonTokenStream(lexer);
+
+			var parser = new SwiftParser(tokens);
+
+			var tree = parser.file();
+
+			var pastwk = new ParseTreeWalker();
+
+			pastwk.Walk(new SwiftSuperListener(args[1]), tree);
+		}
+	}
+}
---
+++ src/SwiftSuperListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SwiftTranslator
+{
+	public class SwiftSuperListener : SwiftBaseListener
+	{
+		readonly string OutputFilename;
+		StreamWriter OutputWriter;
+
+		public SwiftSuperListener(string outputFilename)
+		{
+			OutputFilename = outputFilename;
+
+			OutputWriter = new StreamWriter(File.Open(OutputFilename, FileMode.Create));
+			OutputWriter.AutoFlush = true;
+		}
+
+		void Out(string s)
+		{
+			OutputWriter.Write(s);
+		}
+
+		void OutLine(string s)
+		{
+			OutputWriter.WriteLine(s);
+		}
+
+		void PrintErrorAndExit(int error_code, string message)
+		{
+			Console.WriteLine($"Error {error_code}: {message}");
+
+			OutputWriter.Close();
+			File.Delete(OutputFilename);
+
+			Environment.Exit(1);
+		}
+	}
+}

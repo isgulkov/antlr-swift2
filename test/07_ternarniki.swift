989f794baef957ded8f5667179ccb49e11a6cfb3
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 10:06:43 +0300

    Remove print expressions from test #7, fix associative ternaries

--- test/07_ternarniki.swift
+++ test/07_ternarniki.swift
@@ -2,7 +2,4 @@
 
 print(13 == 13 ? "expected" : "unexpected")
 
-print(14 == 14 ? 13 == 14 : 88 == 88 ? "unexpected" : "expected")
-
-88 == 13 ? print("unexpected")
-18 == 18 ? print("expected")
+print(13 == 14 ? "unexpected" : 88 == 87 ? "unexpected" : 37 == 37 ? "expected" : "unexpected")

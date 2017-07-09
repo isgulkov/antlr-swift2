61a71354efab1a3c71f0bc89be292de792cb09fd
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-09 17:16:29 +0300

    Add test for variable declarations

---
+++ test/02_obyavleniya.swift
@@ -0,0 +1,17 @@
+
+
+var f : Bool = true;
+print("f equals", f)
+
+var g : string = "п͉͎̆р̵̳͔̟͇̜̓̾̂и͇͔͇̲̊̊̆ͪв̪̆";
+print("g equals", g)
+
+var п͉͎̆р̵̳͔̟͇̜̓̾̂и͇͔͇̲̊̊̆ͪв̪̆: string = "1337";
+print("п͉͎̆р̵̳͔̟͇̜̓̾̂и͇͔͇̲̊̊̆ͪв̪̆ equals ", п͉͎̆р̵̳͔̟͇̜̓̾̂и͇͔͇̲̊̊̆ͪв̪̆)
+
+repeat {
+    var ds : String = "otec tramala"
+
+    print("ds declared inside a loop equals", ds);
+} while(false);
+

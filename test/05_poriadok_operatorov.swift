5d6f966456da5ce9843e323385bbbbc6a6a10578
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-09 19:12:15 +0300

    Fix spaces in test #5

--- test/05_poriadok_operatorov.swift
+++ test/05_poriadok_operatorov.swift
@@ -1,8 +1,8 @@
 
 var x : Bool = true || false && false;
 
-print("true || false && false =", x, "(true expected)")
+print("true || false && false = ", x, " (true expected)")
 
 x = (true || false) && false;
 
-print("(true || false) && false =", x, "(false expected)")
+print("(true || false) && false = ", x, " (false expected)")
